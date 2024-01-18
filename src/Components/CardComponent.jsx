d07e9466351239dd776dfc768c456b52fde56d42
Georgi Tsankov <145467172+GeorgiGeorgievTsankov@users.noreply.github.com>
2024-01-18 18:07:43 +0200

    Update CardComponent.jsx

--- src/Components/CardComponent.jsx
+++ src/Components/CardComponent.jsx
@@ -1,1 +1,21 @@
 
+import React from 'react'
+import './CardComponent.css'
+
+
+function CardComponent({recipe}) {
+  return (
+    <div className="card-component">
+           {recipe.map((recipes) => (
+            <div className="card">
+             <h1>{recipes.title}</h1>
+             <img className='card-image' src={recipes.img} alt={recipes.name} />
+             <p>{recipes.body}</p>
+             <button className="btn">Read more..</button>
+             </div>
+           ))};
+    </div>
+  )
+}
+
+export default CardComponent
