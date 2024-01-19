03c704a0b73cdef0bba4b3cebfb0b8f7574d830c
Georgi Tsankov <145467172+GeorgiGeorgievTsankov@users.noreply.github.com>
2024-01-19 14:36:29 +0200

    Update HomeComponent.jsx

--- src/Components/HomeComponent.jsx
+++ src/Components/HomeComponent.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import CardComponent from './CardComponent'
+import useFetch from './useFetch'
 // import salmon from './Assets/salmon.jpg'
 // import burger from './Assets/burger.jpg'
 // import pasta from './Assets/pasta.jpg'
@@ -8,27 +9,11 @@ import CardComponent from './CardComponent'
 
 const HomeComponent = () => {
 
-    const [recipe, setName] = useState(null);
-    const [isPending, setIsPending] = useState(true);
-
-
+    const {data:recipe,isPending}= useFetch('http://localhost:8000/recipe');
     // const handleDelete = (id) => {
     //     const newRecipeData = recipe.filter((recipe) => recipe.id !== id);
     //     setName(newRecipeData);
     // }
-
-    useEffect(() => {
-     setTimeout(() =>{
-        fetch('http://localhost:8000/recipe')
-        .then(res => {
-            return res.json();
-        })
-        .then(data => {
-            setName(data)
-            setIsPending(false)
-        });
-     },[1000])
-    }, []);
 
     return (
         <div className="home-component">
