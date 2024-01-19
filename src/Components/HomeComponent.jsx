3d561ac6cbf7a26afcca75584a2a144608bae420
Georgi Tsankov <145467172+GeorgiGeorgievTsankov@users.noreply.github.com>
2024-01-19 11:05:33 +0200

    Update HomeComponent.jsx
    Add setTimeout()

--- src/Components/HomeComponent.jsx
+++ src/Components/HomeComponent.jsx
@@ -1,4 +1,3 @@
-
 import React, { useEffect, useState } from 'react'
 import CardComponent from './CardComponent'
 // import salmon from './Assets/salmon.jpg'
@@ -10,6 +9,7 @@ import CardComponent from './CardComponent'
 const HomeComponent = () => {
 
     const [recipe, setName] = useState(null);
+    const [isPending, setIsPending] = useState(true);
 
 
     // const handleDelete = (id) => {
@@ -18,18 +18,22 @@ const HomeComponent = () => {
     // }
 
     useEffect(() => {
+     setTimeout(() =>{
         fetch('http://localhost:8000/recipe')
-            .then(res => {
-                return res.json();
-            })
-            .then(data => {
-                setName(data)
-            });
+        .then(res => {
+            return res.json();
+        })
+        .then(data => {
+            setName(data)
+            setIsPending(false)
+        });
+     },[1000])
     }, []);
 
     return (
         <div className="home-component">
             <h1>MyBlogProject</h1>
+            {isPending && <div>Loading....</div>}
             {recipe && <CardComponent recipe={recipe} />}
         </div>
 
