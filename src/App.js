77da56ccac79359a2edcc62cfbc4e9cdb463c01a
Georgi Tsankov <145467172+GeorgiGeorgievTsankov@users.noreply.github.com>
2024-01-20 10:38:40 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import './App.css';
+import { AddContentComponent } from './Components/AddContentComponent';
 import HomeComponent from './Components/HomeComponent';
 import NavBarComponent from './Components/NavBarComponent';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 
 function App() {
   return (
-    <div className="App">
-      <NavBarComponent />
-      <HomeComponent />
-      
-    </div>
+    <Router>
+      <div className="App">
+        <NavBarComponent />
+        <Switch>
+          < Route exact path="/">
+            <HomeComponent />
+          </Route>
+          < Route path="/add-content">
+            <AddContentComponent />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
