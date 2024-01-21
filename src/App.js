694927a8bd8c9eb1bfe821f084869a5599a2aa08
Georgi Tsankov <145467172+GeorgiGeorgievTsankov@users.noreply.github.com>
2024-01-21 19:17:00 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+
 import './App.css';
 import { AddContentComponent } from './Components/AddContentComponent';
 import HomeComponent from './Components/HomeComponent';
 import NavBarComponent from './Components/NavBarComponent';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { RecipeDetailsComponent } from './Components/RecipeDetailsComponent';
+import { NotFoundComponent } from './Components/NotFoundComponent';
 
 
 function App() {
@@ -16,6 +19,12 @@ function App() {
           </Route>
           < Route path="/add-content">
             <AddContentComponent />
+          </Route>
+          < Route path="/recipe/:id">
+            <RecipeDetailsComponent />
+          </Route>
+          < Route path="*">
+            <NotFoundComponent />
           </Route>
         </Switch>
       </div>
